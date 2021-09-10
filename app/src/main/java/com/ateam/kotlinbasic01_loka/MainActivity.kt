10c0a6317b46aa6ef4e82fb8fd5a6f43643ad9fd
Loka017 <jessinthespace@gmail.com>
2021-09-10 19:37:13 +0900

    Toast : 사용자에게 간단한 메세지 보여 줌

--- app/src/main/java/com/ateam/kotlinbasic01_loka/MainActivity.kt
+++ app/src/main/java/com/ateam/kotlinbasic01_loka/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ateam.kotlinbasic01_loka
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +18,11 @@ class MainActivity : AppCompatActivity() {
 //            Ctrl + /
             Log.d("메인화면로그", "클릭용 버튼 눌림")// 디버그
             Log.e("메인화면로그", "에러관련 로그")// 에러
+        }
+
+        smallBtn.setOnClickListener {
+            Toast.makeText(this, "작은 버튼이 눌림", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
