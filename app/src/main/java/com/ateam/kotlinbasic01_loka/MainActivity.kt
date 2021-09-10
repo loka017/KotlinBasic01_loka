01f67e7f87cf86f0952dff9e6b1942d06177b573
Loka017 <jessinthespace@gmail.com>
2021-09-10 19:19:50 +0900

    버튼 2개 추가 -> 기초 이벤트에 사용할 예정

--- app/src/main/java/com/ateam/kotlinbasic01_loka/MainActivity.kt
+++ app/src/main/java/com/ateam/kotlinbasic01_loka/MainActivity.kt
@@ -2,10 +2,14 @@ package com.ateam.kotlinbasic01_loka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
     }
 }
