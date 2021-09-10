1144b5c7bd1f2586b0b02fd59061fc29f71803b4
Loka017 <jessinthespace@gmail.com>
2021-09-10 19:28:41 +0900

    로그 찍어보기

--- app/src/main/java/com/ateam/kotlinbasic01_loka/MainActivity.kt
+++ app/src/main/java/com/ateam/kotlinbasic01_loka/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ateam.kotlinbasic01_loka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        clickBtn.setOnClickListener {
+
+//                    코드에 메모남기기-주석
+//            Ctrl + /
+            Log.d("메인화면로그", "클릭용 버튼 눌림")
+        }
     }
 }
