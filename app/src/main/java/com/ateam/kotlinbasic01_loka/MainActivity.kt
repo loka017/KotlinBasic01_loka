25ffb27db0be9dfde3f3ddaf5a3ea25e1ad2261e
Loka017 <jessinthespace@gmail.com>
2021-09-10 22:40:32 +0900

    문법

--- app/src/main/java/com/ateam/kotlinbasic01_loka/MainActivity.kt
+++ app/src/main/java/com/ateam/kotlinbasic01_loka/MainActivity.kt
@@ -12,6 +12,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//      문법 연습 (조건문)
+
+//        나이를 확인해서 -> 성인인지 / 아닌지
+
+        val userAge = 18
+        if(userAge >= 20){
+            Log.d("성인판별","성인이 맞습니다.")
+        }
+        else if(userAge >= 17){
+            Log.d("성인판별","고등학생")
+        }
+        else{
+            Log.d("성인판별","성인이 아닙니다.")
+        }
+
+
+
+//      문법 연습(변수)
+
+//        변수 만들기
+//        val myName :  String    // value : 상수
+//        var myAge : Int         // variable : 변수
+
+        val myName = "강중걸"
+
+        Log.d("변수값",myName)
+
         clickBtn.setOnClickListener {
 
 //                    코드에 메모남기기-주석
